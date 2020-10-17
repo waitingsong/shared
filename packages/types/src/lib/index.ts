e513ad097b399f99772db1b459ac1883065a6f3a
waiting <1661926154@qq.com>
2020-10-18 01:29:58 +0800

    feat(types): add template-literal.ts
    - StrSplit
    - StrJoin
    - SnakeToCamel
    - SnakeToPascal

--- packages/types/src/lib/index.ts
+++ packages/types/src/lib/index.ts
@@ -5,6 +5,7 @@ export * from './data'
 export * from './npm-pkg'
 export * from './union2tuple'
 export * from './tuple'
+export * from './template-literal'
 export * from './database/common'
 export * from './database/join'
 
---
+++ packages/types/src/lib/template-literal.ts
@@ -0,0 +1,37 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+// ref: https://github.com/microsoft/TypeScript/pull/40336#issuecomment-684122600
+
+type V = string | number | boolean | bigint
+
+export type StrSplit<S extends string, D extends string> =
+  string extends S ? string[] :
+    S extends `${infer T}${D}${infer U}` ? [T, ...StrSplit<U, D>] :
+      [S]
+
+export type StrJoin<T extends V[], D extends string> =
+  T extends [] ? '' :
+    T extends [unknown] ? `${T[0]}`:
+      // @ts-expect-error
+      T extends [unknown, ...infer U] ? `${T[0]}${D}${StrJoin<U, D>}` :
+        string
+
+// export type StrJoin<T extends V[], D extends string> =
+//   T extends [] ? '' :
+//     T extends [V] ? `${T[0]}`: `${T[0]}${D}${StrJoin<TupleTail<T>, D>}`
+
+type FormatCapitalize<T extends V[]> =
+  T extends [] ? [] :
+    T extends [string] ? [`${Capitalize<T[0]>}`] :
+      T extends [string, ...infer U] ? [`${Capitalize<T[0]>}`, ...FormatCapitalize<U & V[]>] :
+        []
+
+type FormatCamelCase<T extends V[]> =
+    T extends [] ? [] :
+      T extends [unknown] ? [T[0]] :
+        T extends [unknown, ...infer U] ? [T[0], ...FormatCapitalize<U & V[]>] :
+          []
+
+export type SnakeToCamel<T extends string, D extends string = '_' | '-'> = StrJoin<FormatCamelCase<StrSplit<T, D>>, ''>
+export type SnakeToPascal<T extends string, D extends string = '_' | '-'> = StrJoin<FormatCapitalize<StrSplit<T, D>>, ''>
+
