fea8f4c31f114890dc4838a0e41a98d0260e3aa7
waiting <1661926154@qq.com>
2021-02-23 14:13:37 +0800

    feat(types): add ValuesOf<T>

--- packages/types/src/lib/common.ts
+++ packages/types/src/lib/common.ts
@@ -129,3 +129,9 @@ export type KnownKeys<T> = {
   [K in keyof T]: string extends K ? never : number extends K ? never : K
 } extends { [_ in keyof T]: infer U } ? U : never
 
+/**
+ * Retrive types
+ * @see https://stackoverflow.com/a/51955852/2887218
+ */
+export type ValuesOf<T> = T extends { [_ in keyof T]: infer U } ? U : never
+
---
+++ packages/types/test/common/10.ValuesOf.test.ts
@@ -0,0 +1,92 @@
+/* eslint-disable node/no-extraneous-import */
+import {
+  basename,
+  join,
+} from '@waiting/shared-core'
+import * as assert from 'power-assert'
+
+import { Equals, ValuesOf } from '../../src/index'
+
+
+const filename = basename(__filename)
+
+interface User1 {
+  uid: bigint
+  age: number
+  gender: number
+  name: string
+  ctime: Date
+  mtime: Date | null
+}
+interface User2 extends User1 {
+  addr: unknown
+}
+interface User3 extends User2 {
+  json: any
+}
+interface User4 extends User2 {
+  neverKey: never
+}
+
+class CUser1 {
+
+  uid: bigint
+
+  age: number
+
+  gender: number
+
+  name: string
+
+  ctime: Date
+
+  mtime: Date | null
+
+}
+class CUser2 extends CUser1 {
+
+  addr: unknown
+
+}
+class CUser3 extends CUser2 {
+
+  json: any
+
+}
+
+describe(filename, () => {
+  describe('should ValuesOf works for class', () => {
+    it('type bigint', () => {
+      type T1 = ValuesOf<CUser1>
+      type Expect = string | bigint | number | Date | null
+      const t1: Equals<T1, Expect> = true
+
+      type T2 = ValuesOf<CUser2>
+      type Expect2 = unknown
+      const t2: Equals<T2, Expect2> = true
+
+      type T3 = ValuesOf<CUser3>
+      type Expect3 = any
+      const t3: Equals<T3, Expect3> = true
+    })
+  })
+
+
+  describe('should ValuesOf works for interface', () => {
+    it('type bigint', () => {
+      type T1 = ValuesOf<User1>
+      type Expect = string | bigint | number | Date | null
+      const t1: Equals<T1, Expect> = true
+
+      type T2 = ValuesOf<User2>
+      type Expect2 = unknown
+      const t2: Equals<T2, Expect2> = true
+
+      type T3 = ValuesOf<User3>
+      type Expect3 = any
+      const t3: Equals<T3, Expect3> = true
+    })
+  })
+
+})
+
