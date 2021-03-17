43f19330a30420622d7a26b5f9f89480d1f94f2b
waiting <1661926154@qq.com>
2021-03-17 21:44:13 +0800

    feat(types): update DateISOString by template-literal-types

--- packages/types/src/lib/alias.ts
+++ packages/types/src/lib/alias.ts
@@ -4,8 +4,15 @@ export type BigIntStr = bigint | string
 
 /**
  * ISO 8601 format,
- * new Date().toISOString(),
- * @example '2020-03-03T02:12:53.136Z'
+ * new Date().toISOString()
+ *
+ * @example
+ * - '2020-03-03T02:12:53.123Z'
  */
-export type DateISOString = string
+export type DateISOString = `${N4}-${N2}-${N2}T${N2}:${N2}:${N2}.${N3}Z`
+
+
+type N4 = `${number}${number}${number}${number}`
+type N3 = `${number}${number}${number}`
+type N2 = `${number}${number}`
 
