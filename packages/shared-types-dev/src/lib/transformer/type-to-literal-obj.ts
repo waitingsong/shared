4ace435d899eff0c75cf084fc5f6feebf1788e79
waiting <1661926154@qq.com>
2021-03-31 16:45:36 +0800

    feat(types-dev): add transTypetoLiteralObj()

---
+++ packages/shared-types-dev/src/lib/transformer/type-to-literal-obj.ts
@@ -0,0 +1,120 @@
+/* eslint-disable max-len */
+// eslint-disable-next-line import/no-extraneous-dependencies
+import ts from 'typescript'
+
+import { createSourceFile } from '../ts-morph/common'
+import {
+  TransFormOptions,
+  TransformCallExpressionToLiteralTypeRet,
+  transformCallExpressionToLiteralType,
+} from '../ts-morph/tpl-literal'
+import {
+  createObjectLiteralExpression,
+  isKeysCallExpression,
+  isKeysImportExpression,
+  processImportDeclaration,
+} from '../ts/common'
+
+import {
+  genTransformerFactor,
+  GenTransformerFactorOpts,
+  VisitNodeOpts,
+} from './common'
+
+
+export interface TransTypetoLiteralObjOpts {
+  needle: string
+  resultType: string
+  importModuleName?: string
+  leadingString: string
+  trailingString: string
+  tsConfigFilePath: string
+  jsPath: string
+  tsPath: string
+}
+
+export interface VOpts extends VisitNodeOpts, TransTypetoLiteralObjOpts {
+  literalRetMap: TransformCallExpressionToLiteralTypeRet
+}
+
+export function transTypetoLiteralObj(
+  program: ts.Program,
+  options: TransTypetoLiteralObjOpts,
+): ts.TransformerFactory<ts.SourceFile> {
+
+  const visitNodeOpts: VOpts = {
+    program,
+    literalRetMap: new Map() as TransformCallExpressionToLiteralTypeRet,
+    ...options,
+  }
+  const opts: GenTransformerFactorOpts<VOpts> = {
+    visitNodeHandler: visitNode,
+    visitNodeOpts,
+  }
+  const transfactory = genTransformerFactor<VOpts>(opts)
+  return transfactory
+}
+
+
+function visitNode(node: ts.SourceFile, options: VOpts): ts.SourceFile
+function visitNode(node: ts.Node, options: VOpts): ts.Node | undefined
+function visitNode(node: ts.Node, options: VOpts): ts.Node | undefined {
+  if (ts.isSourceFile(node)) {
+    if (! options.literalRetMap.size) {
+      const path = node.fileName
+      const file = createSourceFile(options.tsConfigFilePath, path)
+      const opts: TransFormOptions = {
+        sourceFile: file,
+        importModuleName: options.importModuleName,
+        needle: options.needle,
+        resultType: options.resultType,
+        leadingString: options.leadingString,
+        trailingString: options.trailingString,
+      }
+      const retMap = transformCallExpressionToLiteralType(opts)
+      options.literalRetMap = retMap
+    }
+    return node
+  }
+
+  const typeChecker = options.program.getTypeChecker()
+  /* istanbul ignore else */
+  if (isKeysImportExpression(node, options.jsPath, options.tsPath)) {
+    const nodeDecl = processImportDeclaration(node, [options.needle])
+    return nodeDecl
+  }
+  if (! isKeysCallExpression(node, typeChecker, options.needle, options.tsPath)) {
+    return node
+  }
+  if (! node.typeArguments || ! node.typeArguments.length) {
+    return ts.factory.createArrayLiteralExpression([])
+  }
+
+  const pNode = node.parent
+  if (pNode.kind === ts.SyntaxKind.VariableDeclaration) {
+    const sym = typeChecker.getSymbolAtLocation(pNode)
+    // @ts-expect-error
+    const { symbol }: { symbol: ts.Symbol | undefined } = pNode
+    const pNodeName = sym
+      ? sym.getName()
+      : symbol ? symbol.getName() : ''
+    const literalObj = options.literalRetMap.get(pNodeName)
+    if (! literalObj) { return node }
+
+    const newNode = createObjectLiteralExpression(literalObj)
+    ts.addSyntheticLeadingComment(
+      newNode,
+      ts.SyntaxKind.MultiLineCommentTrivia,
+      `${options.leadingString} `,
+    )
+    ts.addSyntheticTrailingComment(
+      newNode,
+      ts.SyntaxKind.MultiLineCommentTrivia,
+      ` ${options.trailingString}`,
+    )
+    return newNode
+  }
+
+  return node
+}
+
---
+++ packages/shared-types-dev/test/transformer-type-to-literal-obj/22.type-to-literal-obj.test.ts
@@ -0,0 +1,122 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import {
+  basename,
+  join,
+  rimraf,
+  writeFileAsync,
+} from '@waiting/shared-core'
+import ts from 'typescript'
+
+import {
+  transTypetoLiteralObj,
+  TransTypetoLiteralObjOpts,
+} from '../../src/lib/transformer/type-to-literal-obj'
+import { expectedDict } from '../literal/config'
+
+// eslint-disable-next-line import/order
+import assert = require('power-assert')
+
+
+const filename = basename(__filename)
+
+describe(filename, () => {
+  const testRetFile = join(__dirname, '../literal/.temp.ts')
+  const compilerOpts = {
+    noEmitOnError: true,
+    noImplicitAny: true,
+    target: ts.ScriptTarget.ESNext,
+    inlineSourceMap: false,
+    module: ts.ModuleKind.CommonJS,
+  }
+  const tsConfigFilePath = join(__dirname, '../../tsconfig.json')
+  const defaultOpts = {
+    tsConfigFilePath,
+    needle: 'genDbDict',
+    resultType: 'DbDict',
+    leadingString: 'eslint-disable',
+    trailingString: 'eslint-enable',
+    jsPath: '',
+  }
+
+  before(async () => {
+    await rimraf(testRetFile)
+  })
+  afterEach(async () => {
+    await rimraf(testRetFile)
+  })
+
+  describe('Should transTypetoLiteralObj works', () => {
+    it('demo1', async () => {
+      const demo = '../literal/demo1.ts.example.ts'
+      const path = join(__dirname, demo)
+      const program = ts.createProgram(
+        [path],
+        compilerOpts,
+      )
+      const file = program.getSourceFile(path)
+      if (! file) {
+        throw new TypeError('sourceFile undefined')
+      }
+
+      const tsPath = join(__dirname, '../literal/dict.ts')
+      const opts: TransTypetoLiteralObjOpts = {
+        ...defaultOpts,
+        importModuleName: '',
+        tsPath,
+      }
+      const tf = transTypetoLiteralObj(program, opts)
+      const result: ts.TransformationResult<ts.SourceFile> = ts.transform<ts.SourceFile>(file, [tf])
+      const [fileRet] = result.transformed
+      if (! fileRet) {
+        assert(false)
+        return
+      }
+      const printer = ts.createPrinter()
+      const codeRet = printer.printFile(fileRet)
+      await writeFileAsync(testRetFile, codeRet)
+
+      const dict = require(testRetFile).dict
+      // console.info({ codeRet, dict })
+      assert.deepStrictEqual(dict, expectedDict)
+    })
+
+    it('demo2', async () => {
+      const demo = '../literal/demo2.ts.example.ts'
+      const path = join(__dirname, demo)
+      const program = ts.createProgram(
+        [path],
+        compilerOpts,
+      )
+      const file = program.getSourceFile(path)
+      if (! file) {
+        throw new TypeError('sourceFile undefined')
+      }
+
+      const tsPath = join(__dirname, '../literal/dict.ts')
+      const opts: TransTypetoLiteralObjOpts = {
+        ...defaultOpts,
+        importModuleName: './dict',
+        tsPath,
+      }
+      const tf = transTypetoLiteralObj(program, opts)
+      const result: ts.TransformationResult<ts.SourceFile> = ts.transform<ts.SourceFile>(file, [tf])
+      const [fileRet] = result.transformed
+      if (! fileRet) {
+        assert(false)
+        return
+      }
+      const printer = ts.createPrinter()
+      const codeRet = printer.printFile(fileRet)
+      await writeFileAsync(testRetFile, codeRet)
+
+      const dict = require(testRetFile).dict
+      // console.info({ codeRet, dict })
+      assert.deepStrictEqual(dict, expectedDict)
+    })
+
+  })
+
+})
+
