6df8fbf80c0237cc7d69b8fe9e2e004f8763124a
waiting <1661926154@qq.com>
2021-05-08 19:38:08 +0800

    chore(core): update comments of assertNeverRx()

--- packages/core/src/lib/asset.ts
+++ packages/core/src/lib/asset.ts
@@ -8,6 +8,9 @@ export function assertNever(x: never): never {
   // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
   throw new Error('Assert Never Unexpected object: ' + x)
 }
+/**
+ * @description use rxjs/NEVER instead
+ */
 export function assertNeverRx(x: never): Observable<never> {
   // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
   return throwError(new Error('Assert Never Unexpected object: ' + x))
