945564b73c42faf665bbe666789e4b7be625db38
waiting <1661926154@qq.com>
2021-06-04 17:09:00 +0800

    feat(types): add types with stats.ts
    - ProcInfo
    - ProcCpuinfo
    - ProcMeminfo
    - ProcStat

--- packages/types/src/lib/index.ts
+++ packages/types/src/lib/index.ts
@@ -4,6 +4,7 @@ export * from './common'
 export * from './data'
 export * from './npm-pkg'
 export * from './union2tuple'
+export * from './stats'
 export * from './tuple'
 export * from './template-literal'
 
