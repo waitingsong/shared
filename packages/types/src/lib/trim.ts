483c28afee21ae4a52bd2da82183da1427d9a696
waiting <1661926154@qq.com>
2022-04-11 14:51:00 +0800

    feat(types): update TrimStart<T, K> support K

--- packages/types/src/lib/trim.ts
+++ packages/types/src/lib/trim.ts
@@ -2,7 +2,7 @@
 /* eslint-disable @typescript-eslint/prefer-ts-expect-error */
 
 
-export type TrimStart<T extends string> = T extends ` ${infer Rest}` ? TrimStart<Rest> : T
+export type TrimStart<T extends string, K extends string = ' '> = T extends `${K}${infer Rest}` ? TrimStart<Rest, K> : T
 export type TrimEnd<T extends string> = T extends `${infer Rest} ` ? TrimEnd<Rest> : T
 export type Trim<T extends string> = TrimStart<TrimEnd<T>>
 
--- packages/types/test/14.trim.test.ts
+++ packages/types/test/14.trim.test.ts
@@ -10,7 +10,7 @@ import {
 
 describe('13.trim.test.ts', () => {
 
-  describe('should TrimStart work', () => {
+  describe('should TrimStart work w/o delimiter', () => {
     it('1', () => {
       type Foo = 'tb_user'
       type T1 = TrimStart<Foo>
@@ -57,6 +57,108 @@ describe('13.trim.test.ts', () => {
       const ret: Equals<T1, ExpectType> = true
       const ret2: Equals<T1, Foo> = false
       const ret3: Equals<T1, '  tb_user'> = false
+      const ret4: Equals<T1, 'tb_user '> = false
+    })
+  })
+
+  describe('should TrimStart work with delimiter space', () => {
+    it('1', () => {
+      type Foo = 'tb_user'
+      type T1 = TrimStart<Foo, ' '>
+      type ExpectType = 'tb_user'
+      const ret: Equals<T1, ExpectType> = true
+      const ret2: Equals<T1, Foo> = true
+      const ret3: Equals<T1, '  tb_user'> = false
+    })
+
+    it('2', () => {
+      type Foo = ' tb_user'
+      type T1 = TrimStart<Foo, ' '>
+      type ExpectType = 'tb_user'
+      const ret: Equals<T1, ExpectType> = true
+      const ret2: Equals<T1, Foo> = false
+      const ret3: Equals<T1, '  tb_user'> = false
+      const ret4: Equals<T1, 'tb_user '> = false
+    })
+
+    it('3', () => {
+      type Foo = ' tb_user '
+      type T1 = TrimStart<Foo, ' '>
+      type ExpectType = 'tb_user '
+      const ret: Equals<T1, ExpectType> = true
+      const ret2: Equals<T1, Foo> = false
+      const ret3: Equals<T1, '  tb_user'> = false
+      const ret4: Equals<T1, 'tb_user  '> = false
+    })
+
+    it('4', () => {
+      type Foo = 'tb_user '
+      type T1 = TrimStart<Foo, ' '>
+      type ExpectType = 'tb_user '
+      const ret: Equals<T1, ExpectType> = true
+      const ret2: Equals<T1, Foo> = true
+      const ret3: Equals<T1, '  tb_user'> = false
+      const ret4: Equals<T1, 'tb_user  '> = false
+    })
+
+    it('5', () => {
+      type Foo = '             tb_user'
+      type T1 = TrimStart<Foo, ' '>
+      type ExpectType = 'tb_user'
+      const ret: Equals<T1, ExpectType> = true
+      const ret2: Equals<T1, Foo> = false
+      const ret3: Equals<T1, '  tb_user'> = false
+      const ret4: Equals<T1, 'tb_user '> = false
+    })
+  })
+
+  describe('should TrimStart work with delimiter _', () => {
+    it('1', () => {
+      type Foo = 'tb_user'
+      type T1 = TrimStart<Foo, '_'>
+      type ExpectType = 'tb_user'
+      const ret: Equals<T1, ExpectType> = true
+      const ret2: Equals<T1, Foo> = true
+      const ret3: Equals<T1, '__tb_user'> = false
+    })
+
+    it('2', () => {
+      type Foo = '_tb_user'
+      type T1 = TrimStart<Foo, '_'>
+      type ExpectType = 'tb_user'
+      const ret: Equals<T1, ExpectType> = true
+      const ret2: Equals<T1, Foo> = false
+      const ret3: Equals<T1, '__tb_user'> = false
+      const ret4: Equals<T1, 'tb_user_'> = false
+    })
+
+    it('3', () => {
+      type Foo = '_tb_user_'
+      type T1 = TrimStart<Foo, '_'>
+      type ExpectType = 'tb_user_'
+      const ret: Equals<T1, ExpectType> = true
+      const ret2: Equals<T1, Foo> = false
+      const ret3: Equals<T1, '__tb_user'> = false
+      const ret4: Equals<T1, 'tb_user__'> = false
+    })
+
+    it('4', () => {
+      type Foo = 'tb_user_'
+      type T1 = TrimStart<Foo, '_'>
+      type ExpectType = 'tb_user_'
+      const ret: Equals<T1, ExpectType> = true
+      const ret2: Equals<T1, Foo> = true
+      const ret3: Equals<T1, '__tb_user'> = false
+      const ret4: Equals<T1, 'tb_user__'> = false
+    })
+
+    it('5', () => {
+      type Foo = '_________tb_user'
+      type T1 = TrimStart<Foo, '_'>
+      type ExpectType = 'tb_user'
+      const ret: Equals<T1, ExpectType> = true
+      const ret2: Equals<T1, Foo> = false
+      const ret3: Equals<T1, '__tb_user'> = false
       const ret4: Equals<T1, 'tb_user '> = false
     })
   })
