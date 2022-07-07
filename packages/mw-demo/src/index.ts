68369adfcb1d976b8d7cb6f75b2d3cdf2a07bf80
waiting <1661926154@qq.com>
2022-07-07 16:50:20 +0800

    chore(boilerplate): add mw-demo

---
+++ packages/mw-demo/src/index.ts
@@ -0,0 +1,27 @@
+
+import {
+  Config,
+  ConfigKey,
+  MiddlewareConfig,
+} from './lib/index'
+
+
+export { AutoConfiguration as Configuration } from './configuration'
+export * from './lib/index'
+export {
+  getComponentConfig,
+  getMiddlewareConfig,
+} from './util/common'
+export * from './middleware/demo.middleware'
+
+
+declare module '@midwayjs/core/dist/interface' {
+  interface MidwayConfig {
+    [ConfigKey.config]: Partial<Config>
+    [ConfigKey.middlewareConfig]: Partial<MiddlewareConfig>
+  }
+  // interface Context {
+  //   jwtState: JwtState
+  // }
+}
+
