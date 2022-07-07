337fcd5e58272258a9c3f75353c001751042ce1a
waiting <1661926154@qq.com>
2022-07-07 13:47:20 +0800

    chore: sync

--- packages/core/test/0.dummy.test.ts
+++ packages/core/test/0.dummy.test.ts
@@ -1,13 +1,15 @@
 import assert from 'node:assert/strict'
 
-import { fileShortPath } from '../src/lib/helper.js'
+import { fileShortPath } from '@waiting/shared-core'
+
+import { testBaseDir } from './root.config.js'
 
 
 describe(fileShortPath(import.meta.url), () => {
 
   describe('should work', () => {
     it('always passed', () => {
-      assert(true)
+      assert(testBaseDir)
     })
   })
 
