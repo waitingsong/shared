43e391992711aa45f17a3a0b7b991074d7cc143d
waiting <1661926154@qq.com>
2022-08-14 23:17:47 +0800

    feat(types): types for join table

---
+++ packages/types/test/join-table/40.join-table.test.ts
@@ -0,0 +1,76 @@
+import { fileShortPath } from '@waiting/shared-core'
+
+import {
+  Equals,
+  DbScopedColsByKey,
+  DbScopedColsByTableType,
+} from '../../src/index.js'
+
+import { DbUser, DbUserExt, DbOrder, Db, UserDO, OrderDO } from './test.model.js'
+
+
+describe(fileShortPath(import.meta.url), () => {
+
+  describe('should DbScopedFiles work ', () => {
+    it('DbUser', () => {
+      type T1 = DbScopedColsByKey<DbUser>
+      type ExpectType = 'tb_user.uid' | 'tb_user.name' | 'tb_user.real_name' | 'tb_user.ctime'
+      const ret: Equals<T1, ExpectType> = true
+    })
+
+    it('DbUserExt', () => {
+      type T1 = DbScopedColsByKey<DbUserExt>
+      type ExpectType = 'tb_user_ext.uid' | 'tb_user_ext.age' | 'tb_user_ext.address'
+      const ret: Equals<T1, ExpectType> = true
+    })
+
+    it('DbOrder', () => {
+      type T1 = DbScopedColsByKey<DbOrder>
+      type ExpectType = 'tb_order.order_id' | 'tb_order.order_name' | 'tb_order.uid' | 'tb_order.ctime'
+      const ret: Equals<T1, ExpectType> = true
+    })
+
+    it('Db', () => {
+      type T1 = DbScopedColsByKey<Db>
+      type ExpectType = 'tb_user.uid' | 'tb_user.name' | 'tb_user.real_name' | 'tb_user.ctime'
+      | 'tb_user_ext.uid' | 'tb_user_ext.age' | 'tb_user_ext.address'
+      | 'tb_order.order_id' | 'tb_order.order_name' | 'tb_order.uid' | 'tb_order.ctime'
+      const ret: Equals<T1, ExpectType> = true
+    })
+
+    it('Db part 1', () => {
+      type T1 = DbScopedColsByKey<Db, 'tb_user'>
+      type ExpectType = 'tb_user.uid' | 'tb_user.name' | 'tb_user.real_name' | 'tb_user.ctime'
+      const ret: Equals<T1, ExpectType> = true
+    })
+
+    it('Db part 2', () => {
+      type T1 = DbScopedColsByKey<Db, 'tb_user' | 'tb_user_ext'>
+      type ExpectType = 'tb_user.uid' | 'tb_user.name' | 'tb_user.real_name' | 'tb_user.ctime'
+      | 'tb_user_ext.uid' | 'tb_user_ext.age' | 'tb_user_ext.address'
+      const ret: Equals<T1, ExpectType> = true
+    })
+  })
+
+  describe('should DbScopedFilesByTableType work', () => {
+    it('all', () => {
+      type T1 = DbScopedColsByTableType<Db>
+      type ExpectType = 'tb_user.uid' | 'tb_user.name' | 'tb_user.real_name' | 'tb_user.ctime'
+      | 'tb_user_ext.uid' | 'tb_user_ext.age' | 'tb_user_ext.address'
+      | 'tb_order.order_id' | 'tb_order.order_name' | 'tb_order.uid' | 'tb_order.ctime'
+      const ret: Equals<T1, ExpectType> = true
+    })
+    it('UserDO', () => {
+      type T1 = DbScopedColsByTableType<Db, UserDO>
+      type ExpectType = 'tb_user.uid' | 'tb_user.name' | 'tb_user.real_name' | 'tb_user.ctime'
+      const ret: Equals<T1, ExpectType> = true
+    })
+    it('UserDO | OrderDO', () => {
+      type T1 = DbScopedColsByTableType<Db, UserDO | OrderDO>
+      type ExpectType = 'tb_user.uid' | 'tb_user.name' | 'tb_user.real_name' | 'tb_user.ctime'
+      | 'tb_order.order_id' | 'tb_order.order_name' | 'tb_order.uid' | 'tb_order.ctime'
+      const ret: Equals<T1, ExpectType> = true
+    })
+  })
+})
+
---
+++ packages/types/test/join-table/test.model.ts
@@ -0,0 +1,45 @@
+import { RecordCamelKeys } from '@waiting/shared-types'
+
+
+export class Db {
+  tb_user: UserDO
+  tb_user_ext: UserExtDO
+  tb_order: OrderDO
+}
+export class DbUser {
+  tb_user: UserDO
+}
+export class DbUserExt {
+  tb_user_ext: UserExtDO
+}
+export class DbOrder {
+  tb_order: OrderDO
+}
+
+export class UserDO {
+  uid: number
+  name: string
+  real_name: string
+  ctime: Date | 'now()'
+}
+
+export class UserExtDO {
+  uid: number
+  age: number
+  address: string | number
+}
+
+export class OrderDO {
+  order_id: string | bigint
+  order_name: string
+  uid: number
+  ctime: Date | 'now()'
+}
+
+export type UserDTO = RecordCamelKeys<UserDO>
+
+export interface Context {
+  uid: number
+  ver: string
+}
+
