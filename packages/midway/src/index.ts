29ca39f9768cbe1986abca83f7497cff2f5a8528
waiting <1661926154@qq.com>
2022-08-30 10:56:47 +0800

    chore(midway): remove packages
    use @mw-components/base instead

--- packages/midway/src/index.ts
+++
@@ -1,4 +0,0 @@
-
-export * from './lib/middleware.js'
-export * from './lib/my-error.js'
-
--- packages/midway/test/root.config.ts
+++
@@ -1,6 +0,0 @@
-import { genCurrentDirname } from '@waiting/shared-core'
-
-
-const CI = !! process.env.CI
-export const testBaseDir = genCurrentDirname(import.meta.url)
-
