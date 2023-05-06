4e28418a3321f06e763658c44e77aa12a927f5bc
waiting <1661926154@qq.com>
2023-05-06 13:54:53 +0800

    feat(types): add types MethodTypeUnknown, AsyncMethodType

--- packages/types/src/lib/func.ts
+++ packages/types/src/lib/func.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unnecessary-type-constraint */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 export interface Func {
@@ -5,4 +6,24 @@ export interface Func {
   (this: any, ...args: any[]): any | Promise<any>
 }
 
+/**
+ * ReturnType is any
+ */
 export type MethodType = (...input: any[]) => (any | Promise<any>)
+
+/**
+ * ReturnType is unknown
+ */
+export type MethodTypeUnknown<
+  ArgsType extends any[] = any[],
+  ReturnType extends unknown = unknown,
+> = (...input: ArgsType) => ReturnType
+
+/**
+ * ReturnType is Promise<unknown>
+ */
+export type AsyncMethodType<
+  ArgsType extends any[] = any[],
+  ResultType extends unknown = unknown,
+> = (...input: ArgsType) => Promise<ResultType>
+
