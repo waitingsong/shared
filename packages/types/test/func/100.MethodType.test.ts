ed24d5b33e7e1446945a1faa96bd6a00ca580cf7
waiting <1661926154@qq.com>
2023-07-24 14:30:34 +0800

    chore(types): fix lint types

--- packages/types/test/func/100.MethodType.test.ts
+++ packages/types/test/func/100.MethodType.test.ts
@@ -12,6 +12,7 @@ describe(fileShortPath(import.meta.url), () => {
       const foo: MethodType = (a: number, b: string): string => {
         return `${a}${b}`
       }
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
       const ret = foo(1, '2')
       const expect = '12'
       assert(ret === expect)
