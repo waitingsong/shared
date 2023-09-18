24d4820d0c37cbd2dc31911131b5f816df653e73
waiting <1661926154@qq.com>
2023-09-18 10:46:51 +0800

    test(core): test/23.cli.test.ts

---
+++ packages/core/test/23.cli.test.ts
@@ -0,0 +1,34 @@
+import assert from 'node:assert/strict'
+
+import { retrieveArgsFromProcess } from '##/index.js'
+import { fileShortPath } from '##/lib/helper.js'
+
+
+describe(fileShortPath(import.meta.url), () => {
+
+  describe('should retrieveArgsFromProcess() work', () => {
+    it('common', async () => {
+      const argv = retrieveArgsFromProcess()
+      console.log({ argv })
+      assert(argv)
+      assert(Array.isArray(argv._), JSON.stringify(argv))
+      assert(argv._[0] === 'test/**/*.test.ts', JSON.stringify(argv))
+      assert(argv.ui === 'bdd', JSON.stringify(argv))
+    })
+
+    it('pass argv', async () => {
+      const argv = retrieveArgsFromProcess(process.argv)
+      assert(argv)
+      assert(Array.isArray(argv._), JSON.stringify(argv))
+      assert(argv._[0] === 'test/**/*.test.ts', JSON.stringify(argv))
+      assert(argv.ui === 'bdd', JSON.stringify(argv))
+    })
+
+    it('pass argv equal default', async () => {
+      const argv = retrieveArgsFromProcess()
+      const argv2 = retrieveArgsFromProcess(process.argv)
+      assert.deepEqual(argv, argv2)
+    })
+  })
+
+})
