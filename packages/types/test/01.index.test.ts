5794b13bbc4659f9b38c2b49978077f414081ddd
waiting <1661926154@qq.com>
2024-01-24 17:01:08 +0800

    chore: sync

--- packages/types/test/01.index.test.ts
+++ packages/types/test/01.index.test.ts
@@ -2,24 +2,19 @@ import assert from 'node:assert/strict'
 
 import { fileShortPath } from '@waiting/shared-core'
 
-import { useComponents } from '##/imports.js'
-import { testConfig } from '#@/root.config.js'
+import * as Src from '##/index.js'
 
 
 describe(fileShortPath(import.meta.url), () => {
 
   describe('should work', () => {
-    it('useComponents ', () => {
-      assert(Array.isArray(useComponents))
-    })
-
-    it('testAppdir eq testConfig.testAppDir', () => {
-      const { app, httpRequest } = testConfig
-      assert(app)
-
-      const appDir = app.getAppDir()
-      assert(appDir)
-      assert(appDir === testConfig.testAppDir)
+    it('foo', () => {
+      assert(typeof Src === 'object', 'typeof Src !== "object"')
+      // @ts-ignore
+      if (typeof Src.foo === 'number') {
+        // @ts-ignore
+        assert(Src.foo === 1)
+      }
     })
   })
 
